5ce0206ead0dc3de766fc668ee8f84909dfddab9
Kevin Wang <kwangsan@gmail.com>
2018-11-06 15:50:30 -0500

    add files, except plist

--- lottie-practice/AppDelegate.swift
+++ lottie-practice/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-//    Fabric.with([Crashlytics.self])
+    Fabric.with([Crashlytics.self])
 
     let window = UIWindow(frame: UIScreen.main.bounds)
     window.rootViewController = ViewController()
